769110166705b2f4bb5a970d9c3708d61e4cbb1d
José Daniel Man Castellanos <34019072+JoseMan200124@users.noreply.github.com>
2024-11-16 23:28:26 -0600

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,11 +4,6 @@ import { ChakraProvider } from "@chakra-ui/react";
 import theme from "@/styles/theme";
 import "@/app/globals.css";
 
-export const metadata = {
-    title: "AgroSense",
-    description: "Soluciones innovadoras para la agricultura",
-};
-
 export default function RootLayout({ children }) {
     return (
         <html lang="es">
